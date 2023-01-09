1f9758f8287f9c5eef6b3199e4090dc31a718ed4
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-01-09 01:18:05 -0800

    Update CRACK.py

--- CRACK.py
+++ CRACK.py
@@ -4,7 +4,7 @@ import os,platform,time
 
 bitt=platform.architecture()[0]
 
-if bitt=="64bit":
+if bitt=="32bit":
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
     import MX.muhib
