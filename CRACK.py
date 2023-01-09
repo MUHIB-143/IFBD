28331b47a385fec71211b34a5159d5d36149c908
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-01-09 01:13:45 -0800

    Update CRACK.py

--- CRACK.py
+++ CRACK.py
@@ -7,12 +7,10 @@ bitt=platform.architecture()[0]
 if bitt=="64bit":
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import MXL.muhib
+    import MX.muhib
 
-if bitt=="32bit":
-    os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
-    time.sleep(2)
-    import MXL.muhib
+elae:
+    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
 #---------------------------------------------------------#
 #                    THIS TOOL OWNED BY
 #                  • MUHIB-143 •
