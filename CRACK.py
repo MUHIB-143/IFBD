2a7a5ad6bc76b98004b87a761f050a9218b86869
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-03-17 23:46:46 +0600

    Update CRACK.py

--- CRACK.py
+++ CRACK.py
@@ -14,7 +14,7 @@ if bitt=="64bit":
 if bitt=="32bit":
     os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import new 
+    import n1w 
  
         
 #---------------------------------------------------------#
