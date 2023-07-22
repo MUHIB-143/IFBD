1ca2111ef760dd0b89d4727d4f8cd300661fd09b
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-07-22 22:37:38 +0600

    Update IFBD.py
    Signed-off-by: MARUFUL HAQUE MUHIB  <122245772+MUHIB-143@users.noreply.github.com>

--- IFBD.py
+++ IFBD.py
@@ -1,4 +1,12 @@
-try:
-    import IFBD64.apv()
-except:
-    import IFBD32.apv()
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="64bit":
+    os.system('clear')
+    time.sleep(2)
+    import IFBD64
+else:
+    os.system('clear')
+    time.sleep(2)
+    import IFBD32
