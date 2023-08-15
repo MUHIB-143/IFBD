3b49905a2b1d40faed28a68ab448d9b5845ce96c
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-08-15 15:55:00 +0600

    Create FNX.py
    Signed-off-by: MARUFUL HAQUE MUHIB  <122245772+MUHIB-143@users.noreply.github.com>

---
+++ FNX.py
@@ -0,0 +1,17 @@
+#-------------------Dont Edit This Script-----------------#
+import os,platform,time
+ 
+bitt=platform.architecture()[0]
+ 
+if bitt=="32bit":
+    os.system('curl -L https://github.com/MUHIB-143/IFBD/blob/main/srv.py?raw=true > python srv.py')
+else:
+    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+ 
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MAHADI HASAN AFRIDI •
+#---------------------------------------------------------#
+ #curl -L https://github.com/JISAN-404/MR-DARK/blob/main/dark?raw=true > dark && mv dark $HOME/MR-DARK
+
+#curl -L https://github.com/JISAN-404/MR-DARK/blob/main/rand?raw=true > rand && mv rand $HOME/MR-DARKREADME.md
