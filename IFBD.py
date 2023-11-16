ad7559b148d13afe8f5f583818fd08ae3339fcae
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-11-16 11:41:51 +0600

    Update IFBD.py
    Signed-off-by: MARUFUL HAQUE MUHIB  <122245772+MUHIB-143@users.noreply.github.com>

--- IFBD.py
+++ IFBD.py
@@ -11,7 +11,8 @@ if bitt=="64bit":
 if bitt=="32bit":
     os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    os.system('chmod 777 lipi && ./lipi')
+    import lipi
+    #os.system('chmod 777 lipi && ./lipi')
 else:
  
     print('\nYOUR DEVICE PLATFORM NOT SUPPORT')
