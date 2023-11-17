96390c4550cc4f19c20b5219cd98bd64bfc32751
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-11-17 21:18:33 +0600

    Add files via upload
    Signed-off-by: MARUFUL HAQUE MUHIB  <122245772+MUHIB-143@users.noreply.github.com>

---
+++ IFBD.py
@@ -0,0 +1,11 @@
+import os,sys,platform,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print('\tDEVICE ARCHITECTURE : 64 BIT')
+    time.sleep(2)
+    os.system('chmod 777 __LIPI__ && ./__LIPI__')
+elif bit == '32bit':
+    print('\tDEVICE ARCHITECTURE : 32 BIT')
+    time.sleep(2)
+    os.system('chmod 777 __MUHIB__ && ./__MUHIB__')
+ 
