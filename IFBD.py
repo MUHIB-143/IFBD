83e35e1d5a4379dc9aa58853502f42be9a31f9b2
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2024-07-30 20:05:36 +0600

    Update IFBD.py
    Signed-off-by: MARUFUL HAQUE MUHIB  <122245772+MUHIB-143@users.noreply.github.com>

--- IFBD.py
+++ IFBD.py
@@ -1,10 +1,10 @@
 import os
 import sys
-import time
+# import time
 def slow(e):
   for i in e + '\n':
     sys.stdout.write(i)
     sys.stdout.flush()
-    time.sleep(0.2)
+    # time.sleep(0.2)
 
 slow("hello world in the webpage using django")
